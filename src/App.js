2da3f69d30a326191c5df7124c5130ba42a4b7ac
Hyeon <dolphin@kakao.com>
2025-09-15 21:12:03 +0900

    삭제구현

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import TodoForm from './components/TodoForm.js';
 import TodoList from './components/TodoList.js';
 
 export default function App({ $target }) {
-    let initialState = ['할일1', '할일2', '할일3'];
+    let initialState = [
+        { id: 1, text: '할일1' },
+        { id: 2, text: '할일2' },
+        { id: 3, text: '할일3' }
+    ];
 
     const onSubmit = text => {
-        const nextState = [...todoList.state, text];
+        const nextState = [...todoList.state, { id: todoList.state.length + 1, text }];
         todoList.setState(nextState);
         console.log(nextState);
     };
+    const onDelete = (id) => {
+		const nextState = todoList.state.filter(row => row.id !== id);
+		todoList.setState(nextState);
+	};
 
     new TodoForm({ $target, onSubmit });
-    const todoList = new TodoList({ $target, initialState });
+    const todoList = new TodoList({ $target, initialState, onDelete });
 }
